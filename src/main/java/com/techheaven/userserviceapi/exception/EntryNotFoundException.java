9eb84b4c9397d06cc0fe83760dca50ea6f5f31ac
AshanHerath <87167360+AshanHerath@users.noreply.github.com>
2024-04-25 10:55:01 +0530

    Create EntryNotFoundException.java

---
+++ src/main/java/com/techheaven/userserviceapi/exception/EntryNotFoundException.java
@@ -0,0 +1,9 @@
+package com.techheaven.userserviceapi.exception;
+
+public class EntryNotFoundException extends RuntimeException {
+
+    public EntryNotFoundException(String message) {
+        super(message);
+    }
+
+}
